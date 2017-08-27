a121ff7c02def68bd9f6ab2afa3abdc2288e809c
Ankita Patil <ankita.patil2@tcs.com>
2017-08-27 19:54:13 +0530

    changes in add cart error

--- myjs.js
+++ myjs.js
@@ -31,3 +31,51 @@ myApp.controller('myLogin', function($scope, $http){
     };
     
 });
+
+
+
+       myApp.controller("myAddToCartCtrl", function($scope,$http){
+       var id=0;
+        var bookname, prize,image;
+var books = [];
+        $scope.myAddToCart = function(obj){
+/*
+    $scope.products.push({name:$scope.name, price:$scope.priceProduct});
+    $scope.nameProduct = "";
+    $scope.priceProduct = "";
+ */         var par=obj.target.parentNode;
+            var node= par.childNodes;
+            quantity = parseInt(node[9].value);
+            $http.get('book.json').then(function(response){
+            var books = response.data.book;
+            console.log(response);
+            for(i=0;i<response.data.book.length;i++){
+            if(parseInt(obj.target.parentNode.id) == response.data.book[i].id){
+            bookname=response.data.book[i].bookname;
+            prize=response.data.book[i].prize;
+            image=response.data.book[i].img;
+            $http.get('cart.json').then(function(cartResponse){
+                console.log(cartResponse);
+                username= "ankitap";
+                var cartUsers= cartResponse.data.cart.user;
+                console.log(cartUsers);
+                for (var j =0 ; j < cartUsers.length;j++){
+                    if(username == cartUsers[j].username){
+                        for(var k=0; k< cartUsers[j].books.length;k++){
+                            if(parseInt(obj.target.parentNode.id)== cartUsers[j].books[k].id){
+                                quantity = quantity + parseInt(cartUsers[j].books[k].quantity);
+                                cartUsers[j].books[k].quantity =quantity;
+                                cartResponse.data.cart.user =cartUsers;
+                                console.log(cartResponse);
+                                $http.post('cart.json', cartResponse);
+
+                            }
+                        }
+                    }
+                }
+            });
+            }
+    }
+        });
+        }
+       });
